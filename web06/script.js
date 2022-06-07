2bb9d906797ab92dcca2209645f10ed178c35629
77_N_Vinh <106954065+dangnhathuyy@users.noreply.github.com>
2022-06-07 10:07:34 +0700

    up web07

--- web06/script.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  $(window).scroll(function () {
-    if ($('body').scrollTop() > 100) {
-      $('.logo').addClass('chucam')
-    } else {
-      $('.logo').removeClass('chucam')
-    }
-  })
-})
