49c3449642d5e97106d0d24c0b35ac4b113f4034
77_N_Vinh <106954065+dangnhathuyy@users.noreply.github.com>
2022-06-07 10:07:05 +0700

    up web06

---
+++ web06/script.js
@@ -0,0 +1,9 @@
+$(function () {
+  $(window).scroll(function () {
+    if ($('body').scrollTop() > 100) {
+      $('.logo').addClass('chucam')
+    } else {
+      $('.logo').removeClass('chucam')
+    }
+  })
+})
